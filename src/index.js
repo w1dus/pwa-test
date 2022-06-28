0f3029102d1a0ffaadbcc5dc2f2367f0b124f743
kimJiYeon <w1dus@hanmail.net>
2022-06-28 15:14:36 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-        <Switch>
-          <Route path={`${process.env.PUBLIC_URL}/`}><App /></Route>
-        </Switch>
-    </Router> 
+    <>       
+      <App />
+    </> 
 );
 
